e2a8f0ad246b4ed55e6ac7ef41dadda9f012869f
sudaoxyz <133337545+sudaoxyz@users.noreply.github.com>
2024-01-25 23:00:56 +0800

    Update and rename config.json to config.js

---
+++ config.js
@@ -0,0 +1,65 @@
+{
+    name: "Boternet",
+    avatar: "http://localhost:5175/favicon.ico",
+    relays: [
+        "wss: //relay.snort.social",
+        "wss://nostr.zoel.network/",
+        "wss://relay.leligobit.link/",
+        "wss://nostr.zkid.social/",
+        "wss://lnbits.aruku.kro.kr/nostrrelay/private"
+    ],
+    follows: [],
+    bots: [
+        {
+            name: "测试脚本testScripts",
+            description: `🔥 [$CHAX](https://twitter.com/search?q=%24CHAX&src=cashtag_click) Burn Update: The flame is rising!
+
+            📅 Latest snapshot: Jan 22 at 00:00:00 UTC
+            
+            🚀 Team efforts: 1 team, 47 fuel additions, 8 new addresses!
+            
+            🔥 Total burned: 223,469 [$CHAX](https://twitter.com/search?q=%24CHAX&src=cashtag_click)
+            
+            🌟 Hats off to [#Grok](https://twitter.com/hashtag/Grok?src=hashtag_click) Bear: 0x99Abd79EC0350E308860dfBd88C59e3D9B4e84ae for initiating this burn!
+            ![image](https://github.com/sudaoxyz/FlappyVault/assets/133337545/67b7ecb5-b927-4af7-b218-fc08a80c2a79)`,
+            author: "Boternet",
+            homepage: "https://www.youhou8.com/scripts/max/%E7%BD%91%E7%9B%98%E8%87%AA%E5%8A%A8%E5%A1%AB%E5%86%99%E5%AF%86%E7%A0%81%E3%80%90%E5%A8%81%E5%8A%9B%E5%8A%A0%E5%BC%BA%E7%89%88%E3%80%91",
+            license: "MIT",
+            version: "0.1.0",
+            twitter: "https://twitter.com/boternet_xyz",
+            url: "https://cdn.jsdelivr.net/gh/sudaoxyz/TheScripts/testScripts.js"
+        },
+        {
+            name: "chax燃烧脚本",
+            description: `🔥 [$CHAX](https://twitter.com/search?q=%24CHAX&src=cashtag_click) burn mechanism
+            🔥 [$CHAX](https://twitter.com/search?q=%24CHAX&src=cashtag_click) burning for action now!
+            
+             ✅Get involved here Burn: 👉[https://chax.world/burn](https://t.co/mZz0seAKrA)  
+            Be a part of this exciting journey and contribute to the [$CHAX](https://twitter.com/search?q=%24CHAX&src=cashtag_click) burn initiative.
+            
+            ![image](https://github.com/sudaoxyz/FlappyVault/assets/133337545/3afc6a39-ee55-4ff6-b618-77283b7e4788)
+            `,
+            author: "Boternet",
+            homepage: "https://www.youhou8.com/scripts/max/%E7%BD%91%E7%9B%98%E8%87%AA%E5%8A%A8%E5%A1%AB%E5%86%99%E5%AF%86%E7%A0%81%E3%80%90%E5%A8%81%E5%8A%9B%E5%8A%A0%E5%BC%BA%E7%89%88%E3%80%91",
+            license: "MIT",
+            version: "0.1.0",
+            twitter: "https://twitter.com/boternet_xyz",
+            url: "https://cdn.jsdelivr.net/gh/sudaoxyz/TheScripts/chaxburn.js"
+        },{
+            name: "Mexc自动下单",
+            description: `We're thrilled to announce that the 
+            [@meme__hy](https://twitter.com/meme__hy)
+             Kickstarter has concluded and [$HY](https://twitter.com/search?q=%24HY&src=cashtag_click) will be listed on [#MEXC](https://twitter.com/hashtag/MEXC?src=hashtag_click)! 
+            
+            🔹Deposit: Opened 
+            🔹HY/USDT Trading: 2024-01-25 15:00 (UTC)
+            ![image](https://github.com/sudaoxyz/FlappyVault/assets/133337545/ee30a393-ae3d-46f1-975c-b88cb23f490b)`,
+            author: "Boternet",
+            homepage: "https://www.youhou8.com/scripts/max/%E7%BD%91%E7%9B%98%E8%87%AA%E5%8A%A8%E5%A1%AB%E5%86%99%E5%AF%86%E7%A0%81%E3%80%90%E5%A8%81%E5%8A%9B%E5%8A%A0%E5%BC%BA%E7%89%88%E3%80%91",
+            license: "MIT",
+            version: "0.1.0",
+            twitter: "https://twitter.com/boternet_xyz",
+            url: "https://cdn.jsdelivr.net/gh/sudaoxyz/TheScripts/chaxburn.js"
+        }
+    ]
+}
