07d111903c8d7e4f93930a66b932d17a8ac0a918
sudaoxyz <133337545+sudaoxyz@users.noreply.github.com>
2024-01-29 23:23:38 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,64 +1,46 @@
-{
-    name: "Boternet",
-    avatar: "http://localhost:5175/favicon.ico",
+a = {
+    extension: "",
+    blacklist: [""],
     relays: [
-        "wss: //relay.snort.social",
+        "wss://relay.snort.social",
         "wss://nostr.zoel.network/",
         "wss://relay.leligobit.link/",
         "wss://nostr.zkid.social/",
         "wss://lnbits.aruku.kro.kr/nostrrelay/private"
     ],
-    follows: [],
+    author: {
+        name: "Boternet",
+        avatar: "/favicon.ico",
+        description: "boternet go!",
+        homepage: "https://twitter.com/boternet_xyz",
+        twitter: "https://twitter.com/boternet_xyz",
+    },
     bots: [
         {
             name: "测试脚本testScripts",
-            description: `🔥 [$CHAX](https://twitter.com/search?q=%24CHAX&src=cashtag_click) Burn Update: The flame is rising!
-
-            📅 Latest snapshot: Jan 22 at 00:00:00 UTC
-            
-            🚀 Team efforts: 1 team, 47 fuel additions, 8 new addresses!
-            
-            🔥 Total burned: 223,469 [$CHAX](https://twitter.com/search?q=%24CHAX&src=cashtag_click)
-            
-            🌟 Hats off to [#Grok](https://twitter.com/hashtag/Grok?src=hashtag_click) Bear: 0x99Abd79EC0350E308860dfBd88C59e3D9B4e84ae for initiating this burn!
-            ![image](https://github.com/sudaoxyz/FlappyVault/assets/133337545/67b7ecb5-b927-4af7-b218-fc08a80c2a79)`,
-            author: "Boternet",
-            homepage: "https://www.youhou8.com/scripts/max/%E7%BD%91%E7%9B%98%E8%87%AA%E5%8A%A8%E5%A1%AB%E5%86%99%E5%AF%86%E7%A0%81%E3%80%90%E5%A8%81%E5%8A%9B%E5%8A%A0%E5%BC%BA%E7%89%88%E3%80%91",
+            description: `这是一个测试脚本,监听tg聊天记录点赞twitter.这是一个测试脚本,监听tg聊天记录点赞twitter`,
+            author: {
+                name: "test user",
+                avatar: "/favicon.ico",
+                description: "boternet go!",
+                homepage: "https://twitter.com/boternet_xyz",
+                twitter: "https://twitter.com/0xChamcha",
+            },
             license: "MIT",
             version: "0.1.0",
-            twitter: "https://twitter.com/boternet_xyz",
-            url: "https://cdn.jsdelivr.net/gh/sudaoxyz/TheScripts/testScripts.js"
+            url: "https://cdn.jsdelivr.net/gh/sudaoxyz/TheScripts/liketwitter.js"
         },
         {
             name: "chax燃烧脚本",
-            description: `<div class="d-block color-fg-default comment-body markdown-body js-comment-body">
-        <p dir="auto">🔥 <a href="https://twitter.com/search?q=%24CHAX&amp;src=cashtag_click" rel="nofollow">$CHAX</a> Burn Update: The flame is rising!</p>
-<p dir="auto">📅 Latest snapshot: Jan 22 at 00:00:00 UTC</p>
-<p dir="auto">🚀 Team efforts: 1 team, 47 fuel additions, 9 new addresses!</p>
-<p dir="auto">🔥 Total burned: 223,469 <a href="https://twitter.com/search?q=%24CHAX&amp;src=cashtag_click" rel="nofollow">$CHAX</a></p>
-<p dir="auto">🌟 Hats off to <a href="https://twitter.com/hashtag/Grok?src=hashtag_click" rel="nofollow">#Grok</a> Bear: 0x99Abd79EC0350E308860dfBd88C59e3D9B4e84ae for initiating this burn!<br>
-<a target="_blank" rel="noopener noreferrer" href="https://github.com/sudaoxyz/FlappyVault/assets/133337545/3afc6a39-ee55-4ff6-b618-77283b7e4788"><img src="https://github.com/sudaoxyz/FlappyVault/assets/133337545/3afc6a39-ee55-4ff6-b618-77283b7e4788" alt="image" style="max-width: 100%;"></a></p>
-    </div>`,
-            author: "Boternet",
-            homepage: "https://www.youhou8.com/scripts/max/%E7%BD%91%E7%9B%98%E8%87%AA%E5%8A%A8%E5%A1%AB%E5%86%99%E5%AF%86%E7%A0%81%E3%80%90%E5%A8%81%E5%8A%9B%E5%8A%A0%E5%BC%BA%E7%89%88%E3%80%91",
+            description: `chax燃烧，自动点击，自动签名确认,chax燃烧，自动点击，自动签名确认aaaaa`,
             license: "MIT",
             version: "0.1.0",
-            twitter: "https://twitter.com/boternet_xyz",
             url: "https://cdn.jsdelivr.net/gh/sudaoxyz/TheScripts/chaxburn.js"
-        },{
+        }, {
             name: "Mexc自动下单",
-            description: `We're thrilled to announce that the 
-            [@meme__hy](https://twitter.com/meme__hy)
-             Kickstarter has concluded and [$HY](https://twitter.com/search?q=%24HY&src=cashtag_click) will be listed on [#MEXC](https://twitter.com/hashtag/MEXC?src=hashtag_click)! 
-            
-            🔹Deposit: Opened 
-            🔹HY/USDT Trading: 2024-01-25 15:00 (UTC)
-            ![image](https://github.com/sudaoxyz/FlappyVault/assets/133337545/ee30a393-ae3d-46f1-975c-b88cb23f490b)`,
-            author: "Boternet",
-            homepage: "https://www.youhou8.com/scripts/max/%E7%BD%91%E7%9B%98%E8%87%AA%E5%8A%A8%E5%A1%AB%E5%86%99%E5%AF%86%E7%A0%81%E3%80%90%E5%A8%81%E5%8A%9B%E5%8A%A0%E5%BC%BA%E7%89%88%E3%80%91",
+            description: `抹茶每隔几十秒自动下单购买chax每次购买1个,抹茶每隔几十秒自动下单购买chax每次购买1个`,
             license: "MIT",
             version: "0.1.0",
-            twitter: "https://twitter.com/boternet_xyz",
             url: "https://cdn.jsdelivr.net/gh/sudaoxyz/TheScripts/mexcAutoBuy.js"
         }
     ]
