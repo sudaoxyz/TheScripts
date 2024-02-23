1149d86d00c691adce296f989773d93b7fce88a3
sudaoxyz <133337545+sudaoxyz@users.noreply.github.com>
2024-02-23 22:50:16 +0800

    Create ms.js

---
+++ ms.js
@@ -0,0 +1,30 @@
+const channelId = '6cddc57ffd43295160a81df273960ba85f421af129b89eebbae9ff024e5afd61'
+const adminNpub = 'npub106fjcjfhmxup4yw4swk7ham96st9emxqd8hjrppeqglva7rvnw2q5edxxs'
+const buildSelector = s => `div[data-testid='cellInnerDiv']:first-child div[data-testid='${s}']`
+
+const authors = [nip19.decode(adminNpub).data]
+const admin = ndk.getUser({ pubkey: authors[0] })
+const follows = await admin.follows({ cacheUsage: "ONLY_RELAY" })
+follows.forEach(user => authors.push(user.pubkey))
+
+const sub = ndk.subscribe({ kinds: [42], since: Date.now() / 1000, authors, "#e": [channelId] })
+sub.on("event", async (event) => {
+    const matched = event.content.match(/冲推\((.*?),(.*?)\)/)
+    if (!!matched) {
+        const xPage = await newPage()
+        await xPage.goto(matched[1])
+        await sleep(1000)
+        try {
+            await sleep(500)
+            await xPage.click(buildSelector('reply'))
+            await sleep(500)
+            await xPage.focus("#layers div.DraftEditor-editorContainer")
+            await sleep(200)
+            await xPage.type("", beautify(matched[2]))
+            await sleep(200)
+            await xPage.click("#layers div[data-testid='tweetButton']")
+        } catch (error) { console.log(error) }
+        await sleep(200)
+        await xPage.close()
+    }
+})
